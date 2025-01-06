c426c21271300e1598b571b0ecc1f1e253ea87b7
Ogoigbe1 <ogoigbe12@gmail.com>
2025-01-06 07:56:22 +0100

    made fix

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,27 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { ProductType } from "@/src/types/type";
 
 type Props = {};
 
 const HomeScreen = (props: Props) => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   useEffect(() => {
     getProducts();
   }, []);
-
+  console.log(products);
   const getProducts = async () => {
-    const URL = "https://localhost:8000/products";
+    const URL = 'https://fakestoreapi.com/products';
+    // const response = await fetch(URL);
     const response = await axios.get(URL);
+    // const data = await response.json();
+    // const data = response.data;
     console.log(response.data);
+    setProducts(response.data);
+    setIsLoading(false);
   };
+  if (isLoading) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Home Screen</Text>
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({index, item }) => <Text>{item.title}</Text>}/>
     </View>
   );
 };
 
-export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
@@ -30,3 +46,4 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
+export default HomeScreen;
